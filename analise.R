22c48584a659014a48c2cb10ce5f926a76b1b4b4
bernardocfranco <bernardocruzfranco@gmail.com>
2026-03-25 19:56:34 -0300

    Adicionei melhorias ao script de arte aleatória em R: - Aumentei o número de pontos para 300. - Incluí uma linha de conexão entre os pontos ordenados. - Adicionei uma grade para melhor visualização. - Criei uma legenda explicando os elementos do gráfico.
    Essas modificações tornam o código mais interessante e o resultado visual mais rico.

--- analise.R
+++ analise.R
@@ -11,3 +11,14 @@ plot(x, y,
      main = "Arte aleatória em R",
      xlab = "Eixo X",
      ylab = "Eixo Y")
+
+lines(sort(x), sort(y), col = "black", lwd = 2)
+
+grid(col = "gray", lty = "dotted")
+
+legend("topright",
+       legend = c("Pontos coloridos", "Linha de conexão"),
+       col = c("red", "black"),
+       pch = c(19, NA),
+       lty = c(NA, 1),
+       lwd = c(NA, 2))
